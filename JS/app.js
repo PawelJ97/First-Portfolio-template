4ceda1e914f5bd8b8c49977127d253861aa03ac9
PJ <jaminski@o2.pl>
2022-06-29 20:55:35 +0200

    add js, href-navigation

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,27 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
+const close_menu = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 
 //navigation - active
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
+
+//background when scrolling
+document.addEventListener('scroll',() =>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 350){
+        header.style.backgroundColor = 'rgba(105, 105, 105,0.887)';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+})
+//close  menu
+close_menu.forEach((item) =>{
+    item.addEventListener('click', ()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
