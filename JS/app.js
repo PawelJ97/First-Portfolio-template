d473aa68a4d45153ace327db33be3ccf7e05fbc0
PJ <jaminski@o2.pl>
2022-06-29 22:07:46 +0200

    footer-changes

--- JS/app.js
+++ JS/app.js
@@ -3,12 +3,11 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
 const close_menu = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 
-//navigation - active
+//navigation - active 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
-
 //background when scrolling
 document.addEventListener('scroll',() =>{
     var scroll_position = window.scrollY;
@@ -18,7 +17,7 @@ document.addEventListener('scroll',() =>{
         header.style.backgroundColor = 'transparent';
     }
 })
-//close  menu
+//close menu
 close_menu.forEach((item) =>{
     item.addEventListener('click', ()=>{
         hamburger.classList.toggle('active');
